9f1eaca5f53186fe8d2b0a60fd5142af10d4a425
Gangyuan <296671396@qq.com>
2018-08-15 21:38:09 +0800

    第二次提交demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,5 @@
 if [ -d $1 ]; then
+  echo 'error: dir exists'
   exit 1
 else
   mkdir $1
@@ -11,5 +12,6 @@ else
   echo "h1{color: red;}" > css/style.css
   echo "var string = "Hello World"
 alert(string)" > js/main.js
+  echo 'success'
   exit 0
 fi
